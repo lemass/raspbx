88cbfc27b07cbf9f544520ea684fbb05e065449e
lemass <lemass@163.com>
2016-01-08 16:29:34 +0800

    dongle cmd dongle0 AT+CMGD=1,4

--- bin/reportClient.py
+++ bin/reportClient.py
@@ -4,9 +4,6 @@
 import time
 import json
 import redis
-import socket
-import fcntl
-import struct
 
 import os
 REDIS_HOST = os.getenv('REDIS_HOST')
@@ -14,6 +11,9 @@ REDIS_PORT = int(os.getenv('REDIS_PORT'))
 REDIS_AUTH = os.getenv('REDIS_AUTH')
 
 ## get local ip address
+import socket
+import fcntl
+import struct
 def get_ip_address(ifname):
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     return socket.inet_ntoa(fcntl.ioctl(
@@ -22,24 +22,29 @@ def get_ip_address(ifname):
         struct.pack('256s', ifname[:15])
     )[20:24])
 
+## get mac address
+import uuid
+def get_mac_address():
+    mac=uuid.UUID(int = uuid.getnode()).hex[-12:]
+    return "server-" + mac
+    #return ":".join([mac[e:e+2] for e in range(0,11,2)])
 
 ##initial redis connection
 pool = redis.ConnectionPool(host=str(REDIS_HOST), port=int(REDIS_PORT), db=0, password=str(REDIS_AUTH))
 r = redis.Redis(connection_pool=pool)
 
 ## report my info
-def report_hosts():
-    localIP = get_ip_address('eth0')
-    ISOTIMEFORMAT = '%Y-%m-%d %X'
-    localDate = time.strftime( ISOTIMEFORMAT, time.localtime() )
-    data = {'hostSet':[{'name':'raspbx', 'ip':localIP, 'date':localDate},{'name':'duck', 'ip':'192.168.2.36', 'date':'2016-01-07 18:43:11'}],'totalCount': 2,'message':'none'}
-    #sethosts = r.set('host',data)
+ISOTIMEFORMAT = '%Y-%m-%d %X'
+localDate = time.strftime( ISOTIMEFORMAT, time.localtime() )
+localName = socket.gethostname()
 
-report_hosts()
+eth = sorted(os.listdir('/sys/class/net/'))
+localIP = []
+if len(eth) > 2:
+    localIP.append(get_ip_address(eth[1]))
+    localIP.append(get_ip_address(eth[0]))
+else:
+    localIP.append(get_ip_address(eth[0]))
 
-gethosts = str(r.get('host')).replace('\'', '\"')
-
-res = json.loads(gethosts)
-print res['hostSet'][0]
-print res['hostSet'][1]
-
+host = {'hostname':localName, 'ips': localIP, 'date':localDate}
+r.set(get_mac_address(),host)
